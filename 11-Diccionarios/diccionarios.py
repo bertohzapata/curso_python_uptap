1ea05f7bf2f1e01ef69e1b65575149d0aa781c4d
Humberto Zapata Leon <humberto.zaleon@gmail.com>
2021-09-02 12:39:31 -0500

    diccionarios

---
+++ 11-Diccionarios/diccionarios.py
@@ -0,0 +1,37 @@
+def run():
+    usuario = {
+        # "key": "value",
+        "id_usuario": 1,
+        "nombre": "Humberto",
+        "apellido_pat": "Zapata",
+        "apellido_mat": "León",
+        "celular": "9621117268",
+        "edad": 26,
+        "id_rol": 2,
+        "salario": 1_000_000.00,
+        "contraseña": "asjdfojoasndiofjnasodfn"
+    }
+    # print(f'{usuario["nombre"]} {usuario["apellido_pat"]} {usuario["apellido_mat"]}')
+
+    # llaves = list(usuario.keys())
+    # print(llaves[2])
+
+    # for key, value in usuario.items():
+    #     print(key,"-",value)
+
+
+
+    poblacion_paises = {
+	'Argentina': 44_938_712,
+	'Brasil': 210_147_125,
+	'Colombia': 50_372_424
+    }
+    # => El país ______ tiene una población de ________
+
+
+
+
+
+
+if __name__ == '__main__':
+    run()
