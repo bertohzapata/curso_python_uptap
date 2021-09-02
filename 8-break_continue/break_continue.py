e751bea042d245998481419862d8ad494784a6b3
Humberto Zapata Leon <humberto.zaleon@gmail.com>
2021-09-02 09:44:11 -0500

    break_continue

---
+++ 8-break_continue/break_continue.py
@@ -0,0 +1,13 @@
+# ejemplo1 Elevador de Japón (CONTINUE)
+# pisos = int(input("Cantidad de pisos: "))
+# for i in range(pisos):
+#     if i+1 == 13:
+#         continue
+#     else:
+#         print("Piso:",i+1)
+
+# Ejemplo 2 BREAK
+for i in range(11_278_123_479_791):
+    print(i)
+    if i == 3:
+        break
---
+++ 8-break_continue/ejercicios.py
@@ -0,0 +1,6 @@
+# Ej1. Utilizar un ciclo while y en cada iteración solicitar un número,
+# solo en caso de que el número capturado sea cero, romper el ciclo con break.
+# De lo contrario continuar la ejecución
+
+
+# Ej2. De un rango del 1 al 1000, imprimir solo los números impares.
