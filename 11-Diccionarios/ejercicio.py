27bba409c24878346c1749aec76f38c75493ffe5
Humberto Zapata Leon <humberto.zaleon@gmail.com>
2021-09-03 09:36:16 -0500

    Ejercicio diccionario

--- 11-Diccionarios/ejercicio.py
+++ 11-Diccionarios/ejercicio.py
@@ -3,19 +3,19 @@
 # y muestra los resultados de calificación por materia
 # así como su promedio
 
-# materias = {"Programación web": 0, "Bases de datos": 0, "Fundamentos de prog.": 0}
-# materias["Programación web"] = 10
-
-
+import random
 
 materias = {"Programación web": 0, "Bases de datos": 0, "Fundamentos de prog.": 0}
-
 promedio = 0
 print(materias)
-for materia, calificacion in materias.items():
-    cal = int(input(f'Dame el valor de la materia {materia} - '))
-    materias[materia] = cal
-    promedio += cal
-promedio = round((promedio/3),2)
+for asignatura, calificacion in materias.items():
+    calificacion = int(random.random() * 100)
+    materias[asignatura] = calificacion
+    promedio += calificacion
+promedio = round(promedio/len(materias),2)
 print(materias)
 print("Promedio:",promedio)
+if promedio >= 70:
+    print("Alumno aprobado")
+else:
+    print("Alumno reprobado")
