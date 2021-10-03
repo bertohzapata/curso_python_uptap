e31c5ea992f66a783d7c657931ef46874353852c
Humberto Zapata Leon <humberto.zaleon@gmail.com>
2021-10-02 19:57:27 -0500

    Manejo de archivos

---
+++ 20-Manejo de archivos/archivos.py
@@ -0,0 +1,25 @@
+import os
+FILE_PATH = os.path.dirname(__file__)
+
+def read():
+    numbers = []
+    with open(f"{FILE_PATH}/archivos/numbers.txt", "r", encoding="utf-8") as f:
+        [numbers.append(int(line)) for line in f]
+    print(numbers)
+
+
+def write():
+    names = ["Humberto", "Edlin", "Kevin", "Antonio", "Alfredo", "Paulina"]
+    with open(f"{FILE_PATH}/archivos/names.txt", "a", encoding="utf-8") as f:
+        for name in names:
+            f.write(name)
+            f.write("\n")
+
+
+def run():
+    # read()
+    write()
+
+
+if __name__ == '__main__':
+    run()
